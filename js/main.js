2cc5c9e28981b31c02da5725e034cd66448c40a7
czross <czr@uw.edu>
2016-05-28 15:46:37 -0700

    I gave up on othe branch...

--- js/main.js
+++ js/main.js
@@ -13,11 +13,15 @@
          * by users can manipulate this. NOTE** This is purely d3 to show where d3 code
          * goes. There is still no communication between Angular and D3 at this point.
          **/
-        var chart = Squares();
 
-        var svg = d3.select(".vis").append("svg")
-            .attr("height", 600)
-            .attr("width", 960);
+        var chart = Squares(".vis");
+
+        var svg = chart.svg();
+
+        d3.json("data/Pre-Title.json", function(error, root) {
+            if (error) throw error;
+            svg.datum(root).call(chart);
+        });
 
         /**
          * This is where we can make changes to the visualization rendered by the code
@@ -27,59 +31,30 @@
         $scope.$watch('step', function() {
             /** See console log for number output as you scroll **/
             console.log($scope.step);
-            if($scope.step == 3) {
+            if($scope.step == 0) {
+                d3.json("data/Pre-Title.json", function(error, root) {
+                    if (error) throw error;
+                    svg.datum(root).call(chart);
+                });
+            }
+            if($scope.step == 1) {
+                d3.json("data/Title.json", function(error, root) {
+                    if (error) throw error;
+                    svg.datum(root).call(chart);
+                });
+            }
+            if($scope.step == 2) {
                 d3.json("data/square1.json", function(error, root) {
                     if (error) throw error;
                     console.log(root);
                     svg.datum(root).call(chart);
                 });
             }
-            if($scope.step == 4) {
+            if($scope.step == 3) {
                 d3.json("data/square2.json", function(error, root) {
                     if (error) throw error;
                     svg.datum(root).call(chart);
                 });
-            }
-            if($scope.step == 5) {
-                var width = 960,
-                    height = 600;
-
-                var rateById = d3.map();
-
-                var quantize = d3.scale.quantize()
-                    .domain([0, .15])
-                    .range(d3.range(9).map(function(i) { return "q" + i + "-9"; }));
-
-                var projection = d3.geo.albersUsa()
-                    .scale(1280)
-                    .translate([width / 2, height / 2]);
-
-                var path = d3.geo.path()
-                    .projection(projection);
-
-                queue()
-                    .defer(d3.json, "./data/us.json")
-                    .defer(d3.tsv, "./data/unemployment.tsv", function(d) { rateById.set(d.id, +d.rate); })
-                    .await(ready);
-
-                function ready(error, us) {
-                    if (error) throw error;
-
-                    svg.append("g")
-                        .attr("class", "counties")
-                        .selectAll("path")
-                        .data(topojson.feature(us, us.objects.counties).features)
-                        .enter().append("path")
-                        .attr("class", function(d) { return quantize(rateById.get(d.id)); })
-                        .attr("d", path);
-
-                    svg.append("path")
-                        .datum(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; }))
-                        .attr("class", "states")
-                        .attr("d", path);
-                }
-
-                d3.select(self.frameElement).style("height", height + "px");
             }
         });
     });
@@ -97,6 +72,5 @@
         };
     });
 })();
-
 
 
