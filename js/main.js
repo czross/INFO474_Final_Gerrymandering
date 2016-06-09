5f2b992f8d358019e309b01a3eb3d7904218969b
Chau Ngo <ngokchau@gmail.com>
2016-06-08 17:00:40 -0700

    remove old sammple map

--- js/main.js
+++ js/main.js
@@ -52,49 +52,7 @@
             if (error) throw error;
             svg.datum(root).call(chart);
         });
-        var width = 960,
-            height = 600;
 
-        var rateById = d3.map();
-
-        var quantize = d3.scale.quantize()
-            .domain([0, .15])
-            .range(d3.range(9).map(function(i) { return "q" + i + "-9"; }));
-
-        var projection = d3.geo.albersUsa()
-            .scale(1280)
-            .translate([width / 2, height / 2]);
-
-        var path = d3.geo.path()
-            .projection(projection);
-
-        var svg = d3.select(".vis2").append("svg")
-            .attr("width", width)
-            .attr("height", height);
-
-        queue()
-            .defer(d3.json, "./data/us.json")
-            .defer(d3.tsv, "./data/unemployment.tsv", function(d) { rateById.set(d.id, +d.rate); })
-            .await(ready);
-
-        function ready(error, us) {
-            if (error) throw error;
-
-            svg.append("g")
-                .attr("class", "counties")
-                .selectAll("path")
-                .data(topojson.feature(us, us.objects.counties).features)
-                .enter().append("path")
-                .attr("class", function(d) { return quantize(rateById.get(d.id)); })
-                .attr("d", path);
-
-            svg.append("path")
-                .datum(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; }))
-                .attr("class", "states")
-                .attr("d", path);
-        }
-
-        d3.select(self.frameElement).style("height", height + "px");
 
         var draw = function(json, state) {
             //Define map projection
